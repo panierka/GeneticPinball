aaaeab8cebf526e73da66d3c63383deb158ead82
Wojciech Barciński <72386943+panierka@users.noreply.github.com>
2023-07-11 18:22:16 +0200

    chore: comments, parameter tuning, bootsplash removal

--- Scripts/Generations/EvolutionaryGenerationProvider.cs
+++ Scripts/Generations/EvolutionaryGenerationProvider.cs
@@ -22,10 +22,14 @@ public partial class EvolutionaryGenerationProvider : BallGenerationProviderNode
     [Export]
     public int EliteClonesAmount { get; set; }
 
+    [Export]
+    public float MutationCoefficient { get; set; } = 1f;
+
     private List<BallParameters>? currentGeneration = null;
 
     public override List<BallParameters> GetGeneration(List<int> scores)
     {
+        // jeśli to pierwsza generacja, stwórz losową
         if (currentGeneration is null)
         {
             currentGeneration = Enumerable
@@ -36,14 +40,20 @@ public partial class EvolutionaryGenerationProvider : BallGenerationProviderNode
             return currentGeneration;
         }
 
+        // skopiuj N elementów, które przejdą do następnej generacji niezmienione
         var clones = currentGeneration
             .Zip(scores)
             .OrderByDescending(x => x.Second)
             .Take(EliteClonesAmount)
             .Select(x => x.First);
 
+        // utwórz potomstwo
         var crossed = Cross(currentGeneration, scores, size - EliteClonesAmount);
+        
+        // zmutuj potomstwo
         var mutated = Mutate(crossed);
+
+        // złącz elity ze zmutowanym potomstwem
         var nextGeneration = clones.Concat(mutated);
 
         currentGeneration = nextGeneration.ToList();
@@ -56,39 +66,44 @@ public partial class EvolutionaryGenerationProvider : BallGenerationProviderNode
         return new List<BallParameters>(parameters)
             .Select(x =>
             {
+                // losowanie - czy mutacja nastąpi?
                 if (!Randomizer.Chance(MutationProbability))
                 {
+                    // jeśli nie, zwróć niezmodyfikowane
                     return x;
                 }
 
+                // jeśli tak, zwróć wersję ze zmienionymi wartościami
                 return x with
                 {
+                    // każda wartość jest zamykana w przedziale (min, max),
+                    // wyznaczonym przez jej dziedzinę - służy do tego metoda Clamp
                     Angle = Mathf.Clamp(
-                        x.Angle + Randomizer.Normal(), 
+                        x.Angle + MutationCoefficient * Randomizer.Normal(), 
                         RandomBallParamatersGenerator.MIN_ANGLE, 
                         RandomBallParamatersGenerator.MAX_ANGLE
                     ),
 
                     InitialVelocity = Mathf.Clamp(
-                        x.InitialVelocity + Randomizer.Normal(), 
+                        x.InitialVelocity + MutationCoefficient * Randomizer.Normal(), 
                         RandomBallParamatersGenerator.MIN_INITIAL_VELOCITY, 
                         RandomBallParamatersGenerator.MAX_INITIAL_VELOCITY
                     ),
                     
                     Mass = Mathf.Clamp(
-                        x.Mass + Randomizer.Normal(), 
+                        x.Mass + MutationCoefficient * Randomizer.Normal(), 
                         RandomBallParamatersGenerator.MIN_MASS, 
                         RandomBallParamatersGenerator.MAX_MASS
                     ),
                     
                     GravityScale = Mathf.Clamp(
-                        x.GravityScale + Randomizer.Normal(), 
+                        x.GravityScale + MutationCoefficient * Randomizer.Normal(), 
                         RandomBallParamatersGenerator.MIN_GRAVITY, 
                         RandomBallParamatersGenerator.MAX_GRAVITY
                     ),
                     
                     SizeScale = Mathf.Clamp(
-                        x.SizeScale + Randomizer.Normal(), 
+                        x.SizeScale + MutationCoefficient * Randomizer.Normal(), 
                         RandomBallParamatersGenerator.MIN_SIZE, 
                         RandomBallParamatersGenerator.MAX_SIZE
                     ),
@@ -99,20 +114,24 @@ public partial class EvolutionaryGenerationProvider : BallGenerationProviderNode
 
     private static List<BallParameters> Cross(List<BallParameters> parameters, List<int> scores, int amount)
     {
+        // stwórz ranking agentów
         var rankedAgents = parameters
             .Zip(scores)
             .Select(x => new BallData { Parameters = x.First, Score = x.Second });
         
         var newGeneration = new List<BallParameters>();
 
+        // wygeneruj pewną ilość potomstwa
         for(int i = 0; i < amount; i++)
         {
+            // wybierz dwóch różnych agentów
             var pair = Roulette(rankedAgents, 2)
                 .Select(x => x.Parameters);
 
             var parent1 = pair.ElementAt(0);
             var parent2 = pair.ElementAt(1);
 
+            // stwórz ich losową wypadkową
             var newAgent = new BallParameters()
             {
                 Angle = Mathf.Clamp(parent1.Angle + Randomizer.Normal() * (parent2.Angle - parent1.Angle), RandomBallParamatersGenerator.MIN_ANGLE, RandomBallParamatersGenerator.MAX_ANGLE),
@@ -130,9 +149,10 @@ public partial class EvolutionaryGenerationProvider : BallGenerationProviderNode
 
     private static IEnumerable<BallData> Roulette(IEnumerable<BallData> pool, int amount)
     {
+        // ustaw malejąco po wyniku losowania, gdzie wagą jest liczba punktów + 1
         return pool
             .OrderByDescending(x => (1 + x.Score) * Randomizer.Range(0, 1))
-            .Take(2);
+            .Take(amount);
     }
 
     private readonly struct BallData
